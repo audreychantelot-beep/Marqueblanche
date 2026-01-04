c324da1c0e3c8e4765a79777d5043cf335b711dd
Audrey Chantelot <audrey.chantelot@gmail.com>
2026-01-04 20:59:27 +0000

    Je ne parles pas du hover mais du background fixe

--- src/components/clients/dialog/ContactSection.tsx
+++ src/components/clients/dialog/ContactSection.tsx
@@ -24,16 +24,16 @@ export function ContactSection({ editedClient, handleChange }: ContactSectionPro
                 <div className="grid grid-cols-2 gap-4">
                     <div className="space-y-2">
                         <Label htmlFor="contactPrincipal.nom" className="text-muted-foreground">Nom</Label>
-                        <Input id="contactPrincipal.nom" name="contactPrincipal.nom" value={editedClient.contactPrincipal.nom || ''} onChange={handleChange} className="border-none" placeholder={placeholderText}/>
+                        <Input id="contactPrincipal.nom" name="contactPrincipal.nom" value={editedClient.contactPrincipal.nom || ''} onChange={handleChange} className="border-none bg-accent" placeholder={placeholderText}/>
                     </div>
                     <div className="space-y-2">
                         <Label htmlFor="contactPrincipal.prenom" className="text-muted-foreground">Prénom</Label>
-                        <Input id="contactPrincipal.prenom" name="contactPrincipal.prenom" value={editedClient.contactPrincipal.prenom || ''} onChange={handleChange} className="border-none" placeholder={placeholderText}/>
+                        <Input id="contactPrincipal.prenom" name="contactPrincipal.prenom" value={editedClient.contactPrincipal.prenom || ''} onChange={handleChange} className="border-none bg-accent" placeholder={placeholderText}/>
                     </div>
                 </div>
                 <div className="space-y-2">
                     <Label htmlFor="contactPrincipal.email" className="text-muted-foreground">Email</Label>
-                    <Input id="contactPrincipal.email" name="contactPrincipal.email" type="email" value={editedClient.contactPrincipal.email || ''} onChange={handleChange} className="border-none" placeholder={placeholderText}/>
+                    <Input id="contactPrincipal.email" name="contactPrincipal.email" type="email" value={editedClient.contactPrincipal.email || ''} onChange={handleChange} className="border-none bg-accent" placeholder={placeholderText}/>
                 </div>
             </div>
         </div>
