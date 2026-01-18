7d1c988d59bf9e43aa61776cdca387335c0c766d
audreychantelot-beep <audrey.chantelot@gmail.com>
2026-01-18 14:54:15 +0100

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7790532048-a7148",
-  "appId": "1:1010913431366:web:b6011da83d55003baddf47",
-  "apiKey": "AIzaSyBZRgYOrruF6MVL2TNioKdH8yqoui01mWk",
-  "authDomain": "studio-7790532048-a7148.firebaseapp.com",
+  "projectId": "",
+  "appId": "",
+  "apiKey": "",
+  "authDomain": "",
   "measurementId": "",
-  "messagingSenderId": "1010913431366"
+  "messagingSenderId": ""
 };
